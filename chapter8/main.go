269915549edc4bd9469376d5e6ba39089c9d20c0
Tom Newark <thomas.newark@phmc.com>
2017-12-13 11:54:37 -0600

    Added examples for Join, Repeat and Replace

--- chapter8/main.go
+++ chapter8/main.go
@@ -25,4 +25,18 @@ func main() {
 	// func Index(s sep string) int
 	fmt.Println(strings.Index("test", "e"))
 	// => 1
+
+	// func Join(a []string, sep string) string 
+	fmt.Println(strings.Join([]string{"a","b"}, "-"))
+	// => a-b
+
+	// func Repeat(s string, count int) string
+	fmt.Println(strings.Repeat("a", 5))
+	// => "aaaaa"
+
+	// func Replace(s, old, new string, n int) string
+	fmt.Println(strings.Replace("aaaa", "a", "b", 2))
+	// => "bbaa"
+
+	
 }
